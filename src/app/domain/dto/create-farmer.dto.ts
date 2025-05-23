19a358e71c312fa7c59a81846495997adb61b1f2
Samuel Prates <samuelprates@yahoo.com.br>
2025-05-23 13:11:47 -0300

    first commit

--- src/app/domain/dto/create-farmer.dto.ts
+++ src/app/domain/dto/create-farmer.dto.ts
@@ -1,12 +1,13 @@
 import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
 import { CreateFarmDto } from './create-farm.dto';
-import { IsString, IsArray, Matches, ValidateNested } from 'class-validator';
+import { IsString, IsArray, Matches, ValidateNested, IsNumberString } from 'class-validator';
 import { Type } from 'class-transformer';
 
 @ApiExtraModels()
 export class CreateFarmerDto {
-    @IsString()
+    @IsNumberString()
     @Matches(/^(\d{11}|\d{14})$/, { message: 'federalIdentification must be a string of 11 digits for CPF or 14 digits for CNPJ', })
+    @ApiProperty()
     federalIdentification: string;
 
     @IsString()
